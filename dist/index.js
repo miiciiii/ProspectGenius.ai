f98b6bf381f1359e45f5ee99bc9ad2340561f8d8
miiciiii <justinjohnmico.09@gmail.com>
2025-09-12 15:51:26 +0800

    fix: javascript issue

--- dist/index.js
+++ dist/index.js
@@ -255,7 +255,8 @@ var vite_config_default = defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/"),
+    // put frontend in dist/public instead of dist/
+    outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
     rollupOptions: {
       external: ["server"]
