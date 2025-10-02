46eee83c0e5b7c300154982cb2da8e5518439adc
miiciiii <justinjohnmico.09@gmail.com>
2025-10-03 00:57:31 +0800

    Complete revamp of the project

--- client/src/components/reports/company-reports/loading.tsx
+++
@@ -1,3 +0,0 @@
-export const Loading: React.FC = () => (
-  <div className="text-center py-10 text-gray-500">Loading company reports...</div>
-);
--- migrations/005_fix_subscription_default.sql
+++
@@ -1,4 +0,0 @@
--- Ensure subscriptions default to 'active' status
--- This fixes any subscriptions that might be created with 'trial' status
-
-ALTER TABLE subscriptions ALTER COLUMN status SET DEFAULT 'active';
